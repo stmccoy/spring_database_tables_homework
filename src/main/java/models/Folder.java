2aa069f6bbea0453f36f41a2125b7b83516ed5d2
crispymccoy145 <samspam19941@gmail.com>
2021-06-22 16:35:32 +0100

    folder object setup

--- src/main/java/models/Folder.java
+++ src/main/java/models/Folder.java
@@ -1,12 +1,66 @@
 package models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 
-
+@Entity
+@Table(name = "folders")
 public class Folder {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long ID;
+
+    @Column(name = "title")
     private String title;
-    private List<File> files;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    @JsonIgnoreProperties("folders")
     private User user;
+
+    @OneToMany
+    @JsonIgnoreProperties({"folder"})
+    private List<File> files;
+
+    public Folder(String title, User user, List<File> files) {
+        this.title = title;
+        this.user = user;
+        this.files = new ArrayList<>();
+    }
+
+    public long getID() {
+        return ID;
+    }
+
+    public void setID(long ID) {
+        this.ID = ID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public List<File> getFiles() {
+        return files;
+    }
+
+    public void setFiles(List<File> files) {
+        this.files = files;
+    }
 }
