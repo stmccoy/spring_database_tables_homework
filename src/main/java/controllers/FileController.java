01ba582aca0606ceb4c099ce7d7971c446469748
crispymccoy145 <samspam19941@gmail.com>
2021-06-22 16:53:11 +0100

    file controller created

---
+++ src/main/java/controllers/FileController.java
@@ -0,0 +1,28 @@
+package controllers;
+
+import models.File;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import repositories.FileRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class FileController {
+
+    @Autowired
+    FileRepository fileRepository;
+
+    @GetMapping(value = "/files")
+    public List<File> getAllFiles(){
+        return fileRepository.findAll();
+    }
+
+    @GetMapping(value = "/files/{id}")
+    public Optional<File> getFile(@PathVariable Long id){
+        return fileRepository.findById(id);
+    }
+}
