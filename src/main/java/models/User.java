d6460f242e809df45632336fd3b118f0ac0fdb66
crispymccoy145 <samspam19941@gmail.com>
2021-06-22 16:37:52 +0100

    user model setup

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -1,12 +1,52 @@
 package models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 
-
+@Entity
+@Table(name = "users")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long ID;
+
+    @Column(name = "name")
     private String name;
+
+    @ManyToOne
+    @JsonIgnoreProperties("user")
     private List<Folder> folders;
 
+    public User(String name, List<Folder> folders) {
+        this.name = name;
+        this.folders = new ArrayList<>();
+    }
+
+    public long getID() {
+        return ID;
+    }
+
+    public void setID(long ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Folder> getFolders() {
+        return folders;
+    }
+
+    public void setFolders(List<Folder> folders) {
+        this.folders = folders;
+    }
 }
